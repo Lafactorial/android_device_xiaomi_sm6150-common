dd8bbbdcfca540f1c2fde873ad0a925a87a793ae
Pranav Vashi <neobuddy89@gmail.com>
2022-06-23 02:22:09 +0530

    sm6150-common: Disable zram writeback
    * Disabled on many devices with reports to lagginess in long term and random reboot in some instances.
    
    Change-Id: I73985159766da08cd35b0288db29cfafc7db62b0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -391,9 +391,3 @@ PRODUCT_PACKAGES += \
 
 # XiaomiParts
 $(call inherit-product, device/xiaomi/sm6150-common/parts/parts.mk)
-
-# ZRAM writeback
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.zram.mark_idle_delay_mins=60 \
-    ro.zram.first_wb_delay_mins=1440 \
-    ro.zram.periodic_wb_delay_hours=24
