6ef10d29c19e569d2fdde44bbba21b85ac450910
rdxzv <rdxzv.dev@gmail.com>
2023-09-03 14:54:10 +0000

    sm6150-common: Import XiaomiParts
    * from https://github.com/rd-stuffs/android_device_xiaomi_surya/tree/topaz-wip/parts
    
    Change-Id: I73ea688478bafe625b11892e4e16901072c890b6
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -432,6 +432,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# XiaomiParts
+$(call inherit-product, device/xiaomi/sm6150-common/parts/parts.mk)
+
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
