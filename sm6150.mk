ad929518b0fe59224b84ad87ee72326a2662a1e7
basamaryan <basam.aryan@gmail.com>
2024-05-29 02:11:18 +0530

    sm6150-common: Switch to the pixel power-libperfmgr
    Change-Id: Ic018a0151f7156eab2bb983e3de52cd7b8756710

--- sm6150.mk
+++ sm6150.mk
@@ -261,7 +261,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
 
 # QTI
@@ -296,7 +296,6 @@ PRODUCT_SOONG_NAMESPACES += \
     bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
