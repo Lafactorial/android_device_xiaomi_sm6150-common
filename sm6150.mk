b29b11b2c6b4e7bf597956b3308b5067778306b7
basamaryan <basam.aryan@gmail.com>
2025-01-02 20:36:43 +0530

    sm6150-common: Bring back QCOM WFD
    This reverts commit 4a838e1a9bde7fe16c802e26f1c4c9d27e8b200f and d5d80a5347fc9a38cf3a5c08ed38a65e565c3b01.
    
    Change-Id: Ib6b2ffc341fc22511ebacdd6b716257090209b3b

--- sm6150.mk
+++ sm6150.mk
@@ -390,6 +390,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# WiFi Display
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
