0a399258678b29162b127c804e7eda5019b42e13
Lafactorial <hknhzl@gmail.com>
2025-02-06 08:57:38 +0300

    sm6150-common:parts: Drop Dirac

--- parts/src/org/lineageos/settings/MainSettingsFragment.java
+++ parts/src/org/lineageos/settings/MainSettingsFragment.java
@@ -11,7 +11,6 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SeekBarPreference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
-import org.lineageos.settings.dirac.DiracActivity;
 import org.lineageos.settings.speaker.ClearSpeakerActivity;
 import org.lineageos.settings.display.KcalSettingsActivity;
 import org.lineageos.settings.refreshrate.RefreshActivity;
@@ -22,13 +21,11 @@ import org.lineageos.settings.utils.HapticUtils;
 
 public class MainSettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener {
 
-    private static final String PREF_DIRAC_SETTINGS = "dirac_settings";
     private static final String PREF_CLEAR_SPEAKER_SETTINGS = "clear_speaker_settings";
     private static final String PREF_KCAL_SETTINGS = "kcal_settings";
     private static final String PREF_REFRESH_RATE_SETTINGS = "refresh_rate_settings";
     private static final String PREF_THERMAL_SETTINGS = "thermal_settings";
 
-    private Preference mDiracSettingsPref;
     private Preference mClearSpeakerSettingsPref;
     private Preference mKcalSettingsPref;
     private Preference mRefreshRateSettingsPref;
@@ -39,13 +36,6 @@ public class MainSettingsFragment extends PreferenceFragment implements OnPrefer
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.xiaomiparts);
-
-        mDiracSettingsPref = (Preference) findPreference(PREF_DIRAC_SETTINGS);
-        mDiracSettingsPref.setOnPreferenceClickListener(preference -> {
-            Intent intent = new Intent(getActivity().getApplicationContext(), DiracActivity.class);
-            startActivity(intent);
-            return true;
-        });
 
         mClearSpeakerSettingsPref = (Preference) findPreference(PREF_CLEAR_SPEAKER_SETTINGS);
         mClearSpeakerSettingsPref.setOnPreferenceClickListener(preference -> {
